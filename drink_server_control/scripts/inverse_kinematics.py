e0b002251437f5e9a6ec5dd2edad3f7dee1e484b
Taishi Mikami <uroko100manen@gmail.com>
2019-11-28 16:03:05 +0900

    Fixed bug

--- drink_server_control/scripts/inverse_kinematics.py
+++ drink_server_control/scripts/inverse_kinematics.py
@@ -4,7 +4,8 @@
 import rospy
 import numpy as np
 import math
-from drink_server_robot.msg import ParamManipulator
+from drink_server_control.msg import ParamManipulator
+from std_msgs.msg import Float64
 
 class ControlManipulator():
     def __init__(self):
@@ -20,8 +21,8 @@ class ControlManipulator():
         self.pub3 = rospy.Publisher('/elbow_servo_controller/command',  Float64, queue_size=10)
         self.pub4 = rospy.Publisher('/wrist_servo_controller/command',  Float64, queue_size=10)
 
-        self.pos_x, self.pos_y, self.pos_z = 0, 0, 0 #ハンド位置の初期値
-        self.post_x, self.post_y, self.post_z = 0, 0, 0 #ハンド姿勢の初期値
+        self.pos_x, self.pos_y, self.pos_z = 200, 0, -30 #ハンド位置の初期値
+        self.post_x, self.post_y, self.post_z = 0, 143, 0 #ハンド姿勢の初期値
         # 各リンクの長さ
         self.l1 = 83
         self.l2 = 93.5
@@ -39,46 +40,55 @@ class ControlManipulator():
         x = self.pos_x
         y = self.pos_y
         z = self.pos_z
-        th4 = self.post_x
+        th4 = math.radians(self.post_y)
 
         # theta1
         th1 = math.atan2(y,x)
         
         #目標座標の計算
         l = math.sqrt(x**2 + y**2)
-        x2 = l - self.l3*math.sin(math.radians(th4))
-        y2 = z - self.l3*math.cos(math.radians(th4))
-
+        x2 = l - self.l3*math.sin(th4)
+        y2 = z - self.l3*math.cos(th4)
+        #print(l,z)
+        #print(x2,y2)
         # theta2
         tmp1 = x2**2 + y2**2 + self.l1**2 - self.l2**2
+        #print(tmp1)
         tmp2 = 2*self.l1*math.sqrt(x2**2 + y2**2)
+        #print(tmp2)
         th2 = math.acos(tmp1/tmp2) + math.atan2(y2, x2)
-        
+        #print(th2)
+        #th2 = th2 - math.pi / 2
         #theta3
         tmp3 = y2 - self.l1*math.sin(th2)
         tmp4 = x2 - self.l1* math.cos(th2)
         th3 = math.atan2(tmp3, tmp4) - th2
-
+        
         #theta4
-        th4 = (th4 - (th3 +  th2)) - math.pi/2
+        #th2 = th2 - math.pi/2
+        th4 = -1*th4 - (th3 + th2)
+        print(th1,th2,th3,th4)
         
         return [th1,th2,th3,th4]
 
     def main(self):
         rad1,rad2,rad3,rad4 = self.inverse_kinematics()
+        print(math.degrees(rad1),math.degrees(rad2 - math.pi/2),math.degrees(rad3),math.degrees(rad4 + math.pi/2))
+
+        #print(rad1,rad2,rad3,rad4)
         #符号忘れたから実機で確認
         self.pub1.publish(rad1)
-        self.pub2.publish(rad2)
-        self.pub3.publish(rad3)
-        self.pub4.publish(rad4)
+        self.pub2.publish(rad2 - math.pi/2)
+        self.pub3.publish(-1*rad3)
+        self.pub4.publish(-1*(rad4 + math.pi/2))
 
         
 
 if __name__ == '__main__':
     rospy.init_node('hand_control')
     rate = rospy.Rate(5)
-    cm = ControlManipulatar()
-    while not rospy.is_shotdown():
+    cm = ControlManipulator()
+    while not rospy.is_shutdown():
         try:
             cm.main()
         except Exception as e:
