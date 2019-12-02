ac85b58f6a48c06141b12569f948b637dd3d0aa1
yukixx6 <yukipass19@gmail.com>
2019-12-02 22:13:40 +0900

    Update program

--- drink_server_control/scripts/hand_controller.py
+++ drink_server_control/scripts/hand_controller.py
@@ -7,14 +7,26 @@ from drink_server_control.msg import ParamManipulator
 from darknet_ros_msgs.msg import BoundingBoxes,BoundingBox
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from std_msgs.msg import Bool, Int16, String
 
 class Talker():
     def __init__(self):
         self.pub = rospy.Publisher('/pos', ParamManipulator, queue_size=1)
+        self.pf_pub = rospy.Publisher('/pf_trigger', Bool, queue_size=1)
+        self.hc_pub = rospy.Publisher('/hc_trigger', Bool, queue_size=1)
+        self.er_pub = rospy.Publisher('/er_trigger', Bool, queue_size=1)
+        self.drink_pub = [
+            rospy.Publisher('drink_server_robot/drink_driver/left_stop', Bool, queue_size=1),
+            rospy.Publisher('drink_server_robot/drink_driver/left_release', Bool, queue_size=1),
+            rospy.Publisher('drink_server_robot/drink_driver/right_stop', Bool, queue_size=1),
+            rospy.Publisher('drink_server_robot/drink_driver/right_release', Bool, queue_size=1)
+        ]
+
         rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.bb_cb)
         rospy.Subscriber('/darknet_ros/detection_image', Image, self.img_cb)
         # rospy.Subscriber('/zed_node/depth/depth_registered', Image, self.dep_cb, queue_size=1) #zed用
         rospy.Subscriber('/camera/depth/image_rect_raw', Image, self.dep_cb, queue_size=1) #realsense用
+        rospy.Subscriber('/emotion', String, self.er_cb)
         self.pm = ParamManipulator()
         self.bridge = CvBridge()
         self.pos_x, self.pos_y, self.pos_z = 10, 0 ,80 #ハンド位置の初期位置
@@ -26,6 +38,11 @@ class Talker():
         self.pos_flag = [False,False] #ハンドの位置の判別用
         self.pos_hold = [0, 0, 0] #ハンドの位置の保持用
         self.ymax = 0
+        self.pf_flag = False
+        self.er_flag = False
+        self.emotion = ""
+        self.drink_section = 0
+        
 
     #バウンディングボックスのコールバック
     def bb_cb(self,data):
@@ -45,13 +62,17 @@ class Talker():
     #画像のコールバック
     def img_cb(self,data):
         self.img = self.bridge.imgmsg_to_cv2(data,'bgr8')
-        cv2.imshow('yolo', self.img)
-        cv2.waitKey(1)
+        # cv2.imshow('yolo', self.img)
+        # cv2.waitKey(1)
 
     #デプス画像のコールバック
     def dep_cb(self,data):
         self.img_dep = []
         self.img_dep = self.bridge.imgmsg_to_cv2(data,'32FC1')
+
+    def er_cb(self,data):
+        self.emotion = data.data
+        #print(self.emotion,"cb")
         
 
     #デプス画像から特定位置のデプスを抜き取る
@@ -74,19 +95,25 @@ class Talker():
         self.pm.post_y = self.post_y
         self.pm.post_z = self.post_z
         self.pub.publish(self.pm)
+        #print(self.emotion)
 
     def main(self):
-        if len(self.cup_pos) != 0:
+        y_count = 0
+        z_count = 0
+        # if len(self.cup_pos) != 0 or self.mode == 1 or self.mode == 2:
+        if len(self.cup_pos) != 0 or self.mode != 0:
             #位置合わせ
             # print(self.cup_pos)
             if self.mode == 0:
                 print("mode : ",self.mode)
                 if (self.cup_pos[0] - self.img.shape[1]/2) < 50:
                     self.pos_y -= 1
+                    y_count -= 1
                     self.pos_flag[0] = False
                     print('左')
                 elif (self.cup_pos[0] - self.img.shape[1]/2) > 100:
                     self.pos_y += 1
+                    y_count += 1
                     self.pos_flag[0] = False
                     print('右')
                 else:
@@ -95,10 +122,12 @@ class Talker():
                 # if (self.ymax - self.img.shape[0]) >  -30:
                 if (self.cup_pos[1] - self.img.shape[0]/2) < -20:
                     self.pos_z += 1
+                    z_count += 1
                     self.pos_flag[1] = False
                     print('上')
                 elif (self.cup_pos[1] - self.img.shape[0]/2) > 70:
                     self.pos_z -= 1
+                    z_count -= 1
                     self.pos_flag[1] = False
                     print('下')
                 else:
@@ -119,7 +148,7 @@ class Talker():
                 if sum(self.pos_flag) == 2:
                     self.mode = 1
                     #現在の位置を保持
-                    # self.pos_hold = [self.pos_y, self.pos_z]
+                    self.pos_hold = [y_count, z_count]
 
             # elif self.mode == 1:
                 # print(self.cup_pos)
@@ -132,21 +161,80 @@ class Talker():
                 #     print('後')
                 # else:
                 #     self.mode = 2
- 
-            #注ぎ動作
-            elif self.mode == 1:
-                print("mode : ",self.mode)
-                self.pos_x, self.pos_y, self.pos_z = 120, 0 ,120 
-                self.post_x, self.post_y, self.post_z = 0, 90, 0 
 
-                #パブリッシュ
+            elif self.mode == 1:
+                self.pos_x, self.pos_y, self.pos_z = 0.85, 0.0, 190.0
+                self.post_x, self.post_y, self.post_z = 0, 60, 0
                 self.pm_publisher()
-                #~~~~~~~~~~~~~~~
-                #飲み物を流す処理
-                #~~~~~~~~~~~~~~~
-                rospy.sleep(3)
-                print("mode change")
-                self.mode = 2
+                rospy.sleep(2)
+                #print("ok")
+                
+
+                # 飲み物１の選択
+                ## 音声を流す
+                if self.drink_section == 0:
+                    # 表情認識
+                    self.er_flag = True
+                    self.er_pub.publish(self.er_flag)
+                    rospy.sleep(2)
+                    
+                    if self.emotion == "HAPPINESS":
+                        self.pos_x, self.pos_y, self.pos_z = 120, 0+self.pos_hold[0] ,120
+                        self.post_x, self.post_y, self.post_z = 0, 90, 0 
+                        self.pm_publisher()
+                        rospy.sleep(2)
+
+                        print("Open drink1")
+                        self.drink_pub[0].publish(0)
+                        rospy.sleep(5)
+                        self.drink_pub[1].publish(1)
+                        rospy.sleep(10)
+                        
+                        print("Close drink1")
+                        self.drink_pub[1].publish(0)
+                        rospy.sleep(1)
+                        self.drink_pub[0].publish(1)
+                        rospy.sleep(5)
+                        
+
+                        self.er_flag = False
+                        self.er_pub.publish(self.er_flag)
+
+                        self.mode = 3
+                    else :
+                        self.drink_section = 1
+
+                elif self.drink_section == 1:
+                    # 表情認識
+                    self.er_flag = True
+                    self.er_pub.publish(self.er_flag)
+                    rospy.sleep(2)
+                    
+                    if self.emotion == "HAPPINESS":
+                        self.pos_x, self.pos_y, self.pos_z = 120, 0+self.pos_hold[0] ,120
+                        self.post_x, self.post_y, self.post_z = 0, 90, 0 
+                        self.pm_publisher()
+                        rospy.sleep(2)
+
+                        print("Open drink2")
+                        self.drink_pub[2].publish(0)
+                        rospy.sleep(5)
+                        self.drink_pub[3].publish(1)
+                        rospy.sleep(10)
+                        
+                        print("Close drink2")
+                        self.drink_pub[3].publish(0)
+                        rospy.sleep(1)
+                        self.drink_pub[2].publish(1)
+                        rospy.sleep(5)
+                        
+
+                        self.er_flag = False
+                        self.er_pub.publish(self.er_flag)
+
+                        self.mode = 3
+                    else :
+                        self.drink_section = 0
 
             #排水動作
             elif self.mode == 2:
@@ -165,7 +253,7 @@ class Talker():
                 rospy.sleep(2)
                 #少し下がる
                 self.pos_x, self.pos_y, self.pos_z = -20, -130 ,0
-                self.post_x, self.post_y, self.post_z = 0, 130, 0
+                self.post_x, self.post_y, self.post_z = 0, 110, 0
                 self.pm_publisher()
                 print("Down")
                 rospy.sleep(2)
@@ -173,7 +261,7 @@ class Talker():
                 self.pos_x, self.pos_y, self.pos_z = -20, -130 ,-70
                 self.post_x, self.post_y, self.post_z = 0, 160, 0
                 self.pm_publisher()
-                rospy.sleep(5)
+                rospy.sleep(10)
 
                 # self.pm.pos_x = self.pos_x
                 # self.pm.pos_y = self.pos_y
@@ -189,9 +277,16 @@ class Talker():
                 self.pm_publisher()
                 rospy.sleep(5)
 
-                self.mode = 5
-
-
+                self.mode = 3
+            
+            if self.mode == 3:
+                self.pf_flag = True
+                m = Main()
+                m.hc_flag = False
+                self.pf_pub.publish(self.pf_flag)
+                self.hc_pub.publish(m.hc_flag)
+                self.mode = 0
+                
         # self.pm.pos_x = self.pos_x
         # self.pm.pos_y = self.pos_y
         # self.pm.pos_z = self.pos_z
@@ -201,15 +296,41 @@ class Talker():
         # self.pub.publish(self.pm)
         self.pm_publisher()
         print("~~~~~~~~~~~~~~~~~")
+        #print(self.emotion)
         # print(self.pm)
+
+class Main():
+    def __init__(self):
+        rospy.Subscriber('/hc_trigger', Bool, self.hc_flag_callback)
+        rospy.Subscriber('/pf_trigger', Bool, self.pf_flag_callback)
+        self.hc_flag = False
+        self.t = Talker()
+    
+    def hc_flag_callback(self,data):
+        self.hc_flag = data.data
+
+    def pf_flag_callback(self,data):
+        self.t.pf_flag = data.data
+
+    def run(self):
+        rate = rospy.Rate(5)
+        while not rospy.is_shutdown():
+            if self.hc_flag == True:
+                try:
+                    self.t.main()
+                except Exception as e:
+                    print(e)
+            rate.sleep()
 
 if __name__ == '__main__':
     rospy.init_node('hand_control')
-    rate = rospy.Rate(5)
-    t = Talker()
-    while not rospy.is_shutdown():
-        try:
-            t.main()
-        except Exception as e:
-            print(e)
-        rate.sleep()
+    # rate = rospy.Rate(5)
+    # t = Talker()
+    # while not rospy.is_shutdown():
+    #     try:
+    #         t.main()
+    #     except Exception as e:
+    #         print(e)
+    #     rate.sleep()
+    m = Main()
+    m.run()
--- drink_server_control/scripts/person.py
+++ drink_server_control/scripts/person.py
@@ -10,6 +10,7 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 from drink_server_control.msg import ParamManipulator
+from std_msgs.msg import Bool
 
 class Person_Follow():
     def __init__(self):
@@ -18,6 +19,8 @@ class Person_Follow():
         rospy.Subscriber('/camera/depth/image_rect_raw', Image, self.dep_cb, queue_size=1) #realsense用
         self.cmd_pub = rospy.Publisher('/drink_server_robot/diff_drive_controller/cmd_vel', Twist, queue_size=1)
         self.pos_pub = rospy.Publisher('/pos', ParamManipulator, queue_size=1)
+        self.hc_pub = rospy.Publisher('/hc_trigger', Bool, queue_size=1)
+        self.pf_pub = rospy.Publisher('/pf_trigger', Bool, queue_size=1)
         self.bridge = CvBridge()
         self.twist = Twist()
         self.img = [720,1280]
@@ -27,39 +30,40 @@ class Person_Follow():
         self.pm = ParamManipulator()
         self.pos_x, self.pos_y, self.pos_z = 10, 0 ,80 #ハンド位置の初期位置
         self.post_x, self.post_y, self.post_z = 0, 90, 0 #ハンド姿勢の初期姿勢
+        self.hc_flag = False
 
 
     def yolo_callback(self,data):
-        box_size = 0 #バウンディングボックスの大きさ
-        gx = 0 #バウンディングボックスの中心座標
-        gy = 0 #バウンディングボックスの中心座標
-        person_box = 0
-        dep_val= 0 
+        self.box_size = 0 #バウンディングボックスの大きさ
+        self.gx = 0 #バウンディングボックスの中心座標
+        self.gy = 0 #バウンディングボックスの中心座標
+        self.person_box = 0
+        self.dep_hold= 0 
 
         for i in data.bounding_boxes:
             if self.mode == 0 or self.mode == 1:
                 if i.Class == "person": #人判別
                     self.mode = 1
-                    if box_size < calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax): #一番近い人を探す
-                        box_size = calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax)
-                        gx,gy = calc_gravity_point(i.xmin,i.ymin,i.xmax,i.ymax)
+                    if self.box_size < calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax): #一番近い人を探す
+                        self.box_size = calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax)
+                        self.gx,self.gy = calc_gravity_point(i.xmin,i.ymin,i.xmax,i.ymax)
                         # print("person")
 
             if self.mode != 3:
                 if i.Class == "cup" or i.Class == "bowl": #コップ判別
                     self.mode = 2
-                    if box_size < calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax): #一番近いコップを探す
-                        box_size = calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax)
-                        gx,gy = calc_gravity_point(i.xmin,i.ymin,i.xmax,i.ymax)
-                        self.dep_listener(gx,gy)
-                        dep_val = self.dep_val
+                    if self.box_size < calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax): #一番近いコップを探す
+                        self.box_size = calc_box_size(i.xmin,i.ymin,i.xmax,i.ymax)
+                        self.gx,self.gy = calc_gravity_point(i.xmin,i.ymin,i.xmax,i.ymax)
+                        self.dep_listener(self.gx,self.gy)
+                        self.dep_hold = self.dep_val
         
-        self.talker(gx,gy,box_size,dep_val)
+        # self.talker(gx,gy,box_size,dep_val)
 
     def image_callback(self,data):
         self.img = self.bridge.imgmsg_to_cv2(data,'bgr8')
-        cv2.imshow('yolo', self.img)
-        cv2.waitKey(1)
+        # cv2.imshow('yolo', self.img)
+        # cv2.waitKey(1)
 
     #デプス画像のコールバック
     def dep_cb(self,data):
@@ -87,29 +91,29 @@ class Person_Follow():
         self.pm.post_z = self.post_z
         self.pos_pub.publish(self.pm)
 
-    def talker(self,x,y,box_size,dep_val):
+    def talker(self):
         self.twist.linear.x = 0
         self.twist.angular.z = 0
-        self.pos_talker()
+        # self.pos_talker()
         
         # 人追従モード
         if self.mode == 1:
             #左右
-            if self.img.shape[1]/2 - x < -30:
-                self.twist.angular.z = -5
+            if self.img.shape[1]/2 - self.gx < -30:
+                self.twist.angular.z = -2
                 print("Right_person")
-            elif self.img.shape[1]/2 > self.img.shape[1]/2 - x > 30:
-                self.twist.angular.z = 5
+            elif self.img.shape[1]/2 > self.img.shape[1]/2 - self.gx > 30:
+                self.twist.angular.z = 2
                 print("Left_person")
             else:
                 self.twist.angular.z = 0
 
             #前進
-            if 0 < box_size <= 150000:
-                self.twist.linear.x = 1
+            if 0 < self.box_size <= 150000:
+                self.twist.linear.x = 0.5
                 print("Go_person")
-            elif box_size > 250000:
-                self.twist.linear.x = -1
+            elif self.box_size > 250000:
+                self.twist.linear.x = -0.5
                 print("Back_person")
             else:
                 self.twist.linear.x = 0
@@ -120,35 +124,34 @@ class Person_Follow():
         #コップ追従モード
         if self.mode == 2:
             #左右
-            print(dep_val)
-            if self.img.shape[1]/2 - x < -150:
-                self.twist.angular.z = -5
+            if self.img.shape[1]/2 - self.gx < -150:
+                self.twist.angular.z = -2
                 print("Right")
-            elif self.img.shape[1]/2 > self.img.shape[1]/2 - x > -50:
-                self.twist.angular.z = 5
+            elif self.img.shape[1]/2 > self.img.shape[1]/2 - self.gx > -50:
+                self.twist.angular.z = 2
                 print("Left")
             else:
                 self.twist.angular.z = 0
 
-            #前進
-            # if 0 < box_size <= 95000:
+            # 前進
+            if 0 < self.box_size <= 90000:
+                self.twist.linear.x = 0.5
+                print("Go")
+            elif self.box_size > 120000:
+                self.twist.linear.x = -0.5
+                print("Back")
+            else:
+                self.twist.linear.x = 0
+            # if self.dep_hold > 180 and self.dep_hold != 0:
             #     self.twist.linear.x = 1
             #     print("Go")
-            # elif box_size > 100000:
+            # elif self.dep_hold < 50 and self.dep_hold != 0:
             #     self.twist.linear.x = -1
             #     print("Back")
             # else:
             #     self.twist.linear.x = 0
-            if dep_val > 180 and dep_val != 0:
-                self.twist.linear.x = 1
-                print("Go")
-            elif dep_val < 50 and dep_val != 0:
-                self.twist.linear.x = -1
-                print("Back")
-            else:
-                self.twist.linear.x = 0
 
-            if (x != 0 and box_size != 0 and self.dep_val != 0) and self.twist.angular.z == 0 and self.twist.linear.x == 0:
+            if (self.gx != 0 and self.box_size != 0 and self.dep_hold != 0) and self.twist.angular.z == 0 and self.twist.linear.x == 0:
                 print("ok")
                 self.mode = 3
 
@@ -163,14 +166,44 @@ class Person_Follow():
                 #     print("ok")
 
         elif self.mode == 3:
-            print(self.twist)
-            print("mode : ",self.mode)
+            m = Main()
+            self.hc_flag = True
+            self.hc_pub.publish(self.hc_flag)
+            # print("mode : ",self.mode)
+            m.pf_flag = False
+            self.pf_pub.publish(m.pf_flag)
+            self.mode = 0
 
         print('~~~~~~~~~~~~~~')
         self.cmd_pub.publish(self.twist)
 
 
+class Main():
+    def __init__(self):
+        rospy.Subscriber('/pf_trigger', Bool, self.pf_flag_callback)
+        rospy.Subscriber('/hc_trigger', Bool, self.hc_flag_callback)
+        self.pf_flag = True
+        self.pf = Person_Follow()
+
+    def pf_flag_callback(self,data):
+        self.pf_flag = data.data
+
+    def hc_flag_callback(self,data):
+        self.pf.hc_flag = data.data
+
+    def run(self):
+        rate = rospy.Rate(5)
+        while not rospy.is_shutdown():
+            if self.pf_flag == True:
+                try:
+                    self.pf.talker()
+                except Exception as e:
+                    print(e)
+            rate.sleep()
+
 if __name__ == '__main__':
     rospy.init_node('parson_follow')
-    Person_Follow()
-    rospy.spin()
+    # Person_Follow()
+    # rospy.spin()
+    m = Main()
+    m.run()
