97583477e7529cadb04e846b79561462af05a600
HarukiTakamura <harki20452036@gmail.com>
2019-12-03 15:35:36 +0900

    Rename

--- drink_server_sh/left_push.sh
+++
@@ -1,2 +0,0 @@
-rostopic pub -1 /drink_server_robot/drink_driver/left_push std_msgs/Bool "data: 1"
-rostopic pub -1 /drink_server_robot/drink_driver/left_push std_msgs/Bool "data: 0"
---
+++ drink_server_sh/right_release.sh
@@ -0,0 +1,2 @@
+rostopic pub -1 /drink_server_robot/drink_driver/right_release std_msgs/Bool "data: 1"
+rostopic pub -1 /drink_server_robot/drink_driver/right_release std_msgs/Bool "data: 0"
